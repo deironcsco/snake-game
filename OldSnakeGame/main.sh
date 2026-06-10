c867a7192e7442b8c2c7ef8d38c55aead2189e15
Connor Slutsky <connorslutsky@gmail.com>
2026-06-10 13:47:57 -0400

    T2: fix: refactor QuitButton and implement objects and registry
    change main.exe to old-main.exe in oldsg main.sh

--- OldSnakeGame/main.sh
+++ OldSnakeGame/main.sh
@@ -1,2 +1,3 @@
-g++ main.cpp -g -IC:\\Users\\conno\\SFML-3.0.0\\include -LC:\\Users\\conno\\SFML-3.0.0\\lib -lsfml-graphics -lsfml-system -lsfml-window -o main.exe
-./main.exe
+g++ main.cpp -g -IC:\\Users\\conno\\SFML-3.0.0\\include -LC:\\Users\\conno\\SFML-3.0.0\\lib -lsfml-graphics -lsfml-system -lsfml-window -o old-main.exe
+# ../bin/old-main.exe?
+./old-main.exe
